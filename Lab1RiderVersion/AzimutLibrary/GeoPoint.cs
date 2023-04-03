997f690b2253ab443a0be4bb6bb9812a69c1a36b
Grigoriy Kiselev <Grigoriy.Kiselev@norbit.ru>
2023-04-03 05:48:43 +0300

    Add test data from labs requirements

--- Lab1RiderVersion/AzimutLibrary/GeoPoint.cs
+++ Lab1RiderVersion/AzimutLibrary/GeoPoint.cs
@@ -1,5 +1,27 @@
 ﻿namespace AzimutLibrary;
 
+/// <summary>
+/// Географическая точка.
+/// </summary>
 public class GeoPoint
 {
+    /// <summary>
+    /// Широта.
+    /// </summary>
+    public double Latitude { get; set; }
+    
+    /// <summary>
+    /// Долгота.
+    /// </summary>
+    public double Longitude { get; set; }
+    
+    public GeoPoint(double latitude, double longitude)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+    
+    public GeoPoint()
+    {
+    }
 }
--- Lab1RiderVersion/AzimutTests/GeoManagerTest.cs
+++ Lab1RiderVersion/AzimutTests/GeoManagerTest.cs
@@ -73,10 +73,10 @@ public class GeoManagerTest
     /// <param name="endPoint">Конечная точка.</param>
     [TestMethod]
     [ExpectedException(typeof(ArithmeticException))]
-    [DynamicData(nameof(GetIncorrectPoints), DynamicDataSourceType.Method)]
-    public void CalculateAzimut_IncorrectPoints_ThrowDistanceException(GeoPoint startPoint, GeoPoint endPoint)
+    [DynamicData(nameof(GetPoints), DynamicDataSourceType.Method)]
+    public void CalculateAzimut_Points_СheckHalfEarthLength(GeoPoint startPoint, GeoPoint endPoint)
     {
-        GeoManager.CalculateAzimut(startPoint, endPoint);
+        Assert.IsTrue(GeoManager.CalculateAzimut(startPoint, endPoint).Distance <= _halfEarthLength);
     }
 
     #endregion
@@ -85,40 +85,108 @@ public class GeoManagerTest
 
     private static IEnumerable<object? []> GetCorrectPointsAzimut()
     {
+        // Точки на экваторе.
         yield return new object? []
         {
-            new GeoPoint {},
-            new GeoPoint {},
+            new GeoPoint(0, 120),
+            new GeoPoint(0, -100),
+            90
+        };
+        
+        // Точки на экваторе.
+        yield return new object? []
+        {
+            new GeoPoint(0, 120),
+            new GeoPoint(0, 10),
+            270
+        };
+        
+        // Идем на север
+        yield return new object? []
+        {
+            new GeoPoint(1, 50),
+            new GeoPoint(45, 50),
             0
+        };
+        
+        // Идем на юг
+        yield return new object? []
+        {
+            new GeoPoint(1, 50),
+            new GeoPoint(-45, 50),
+            180
+        };
+        
+        // Случайные точки.
+        yield return new object? []
+        {
+            new GeoPoint(37, 67),
+            new GeoPoint(-42, -87),
+            247.97
         };
     }
     
     private static IEnumerable<object? []> GetCorrectPointsDistance()
     {
+        // Случайные точки.
         yield return new object? []
         {
-            new GeoPoint {},
-            new GeoPoint {},
-            0
+            new GeoPoint(37, 67),
+            new GeoPoint(-42, -87),
+            12532.95
+        };
+        
+        // Случайные точки.
+        yield return new object? []
+        {
+            new GeoPoint(87, 67),
+            new GeoPoint(-42, -87),
+            11735.77
         };
     }
     
-    private static IEnumerable<object? []> GetIncorrectPoints()
+    private static IEnumerable<object? []> GetPoints()
     {
+        // Проверка на то, что расстояние между точками не может быть больше половины экватора.
         yield return new object? []
         {
-            new GeoPoint {},
-            new GeoPoint {}
+            new GeoPoint(0, 179),
+            new GeoPoint(0, -179)
         };
     }
     
     private static IEnumerable<object? []> GetIncorrectPointsUnpossibleAzimut()
     {
+        // Совпадающие точки.
         yield return new object? []
         {
-            new GeoPoint {},
-            new GeoPoint {},
+            new GeoPoint(5, 5),
+            new GeoPoint(5, 5),
             UnpossibleAzimut.None
+        };
+        
+        // Точки, лежащие на прямой через центр земли.
+        yield return new object? []
+        {
+            new GeoPoint(0, 0),
+            new GeoPoint(0, 180),
+            UnpossibleAzimut.Any
+        };
+        
+        // Точки, лежащие на одном полюсе.
+        yield return new object? []
+        {
+            new GeoPoint(90, 0),
+            new GeoPoint(90, 50),
+            UnpossibleAzimut.None
+        };
+        
+        // Точки, лежащие на противоположных полюсах.
+        yield return new object? []
+        {
+            new GeoPoint(90, 0),
+            new GeoPoint(-90, 0),
+            UnpossibleAzimut.Any
         };
     }
 
